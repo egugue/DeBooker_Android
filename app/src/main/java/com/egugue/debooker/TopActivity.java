53b3775f2ee5782a6cf87fe63d65aec2315782f1
Haruyoshi Toyama <duane0728@gmail.com>
2017-01-05 23:03:33 +0900

    Initial commit

---
+++ app/src/main/java/com/egugue/debooker/TopActivity.java
@@ -0,0 +1,13 @@
+package com.egugue.debooker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_top);
+  }
+}
